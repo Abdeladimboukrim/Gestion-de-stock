05feb8444e3399caa6d80dbbcd8ca51b867f5e03
abdeladimboukrim <boukrimabdeladim@gmail.com>
2021-07-27 10:33:29 +0100

    Create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel gestion de stock</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    </head>
+    <body>
+    <div class="container" style="margin-top: 50px;">
+
+<h3 class="text-center text-danger"><b>gestion de stock</b> </h3>
+<a href="/create" class="btn btn-outline-success">Add New Post</a>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Description</th>
+        <th>Cover</th>
+        <th>Update</th>
+        <th>Delete</th>
+    </tr>
+    </thead>
+    
+</table>
+</div>
+
+
+
+    </body>
+</html>
