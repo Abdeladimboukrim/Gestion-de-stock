bcefb957ebe3882b575c38bb3226afb55f71a9c4
abdeladimboukrim <boukrimabdeladim@gmail.com>
2021-08-16 13:11:36 +0100

    Fix insertion des produits

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Image;
+use App\Models\Images;
 class Produit extends Model
 {
     use HasFactory;
